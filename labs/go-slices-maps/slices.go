4fa4dc9e278e95ff8fcc2b23619631088f717504
guille <guille@levita.app>
2020-01-28 21:56:23 -0600

    go-slices-maps hw done
    Signed-off-by: guille <guille@levita.app>

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,19 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	sy := make([][]uint8, dy)
+	for y := range sy {
+		sx := make([]uint8, dx)
+		for x := range sx {
+			sx[x] = uint8(x ^ y)
+		}
+		sy[y] = sx
+	}
+	return sy
+}
+
 func main() {
+	pic.Show(Pic)
 }
