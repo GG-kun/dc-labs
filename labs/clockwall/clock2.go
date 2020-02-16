4572ebf7aef69fdb25d5db284bf51a41e3a960f4
guille <guille@levita.app>
2020-02-15 18:46:48 -0600

    ClockWall challenge aun le falta el TZ

--- labs/clockwall/clock2.go
+++ labs/clockwall/clock2.go
@@ -2,25 +2,42 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"time"
 )
 
-func handleConn(c net.Conn) {
+func handleConn(c net.Conn, name string) {
 	defer c.Close()
-	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
-		if err != nil {
-			return // e.g., client disconnected
-		}
-		time.Sleep(1 * time.Second)
+	t, err := timeIn(time.Now(), name)
+	if err != nil {
+		return // e.g., client disconnected
 	}
+	_, err = io.WriteString(c, t.Format("15:04:05\n"))
+	if err != nil {
+		return // e.g., client disconnected
+	}
+	time.Sleep(1 * time.Second)
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+
+	var port = flag.String("port", "", "")
+	//var tz = flag.String("TZ", "", "")
+	flag.Parse()
+	fmt.Print(*port)
+	listenerPort("US/Eastern", *port)
+
+	/*TZ=US/Eastern    ./clock2 -port 8010 &
+	TZ=Asia/Tokyo    ./clock2 -port 8020 &
+	TZ=Europe/London ./clock2 -port 8030 &*/
+}
+
+func listenerPort(name, port string) {
+	listener, err := net.Listen("tcp", "localhost:"+port)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,6 +47,14 @@ func main() {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn) // handle connections concurrently
+		go handleConn(conn, name) // handle connections concurrently
 	}
+}
+
+func timeIn(t time.Time, name string) (time.Time, error) {
+	loc, err := time.LoadLocation(name)
+	if err == nil {
+		t = t.In(loc)
+	}
+	return t, err
 }
---
+++ labs/clockwall/clockWall.go
@@ -0,0 +1,34 @@
+// Netcat1 is a read-only TCP client.
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	for _, arg := range args {
+		port := strings.Split(arg, ":")[1]
+		dialPort(port)
+	}
+	//clockWall NewYork=localhost:8010 Tokyo=localhost:8020 London=localhost:8030
+}
+
+func dialPort(port string) {
+	conn, err := net.Dial("tcp", "localhost:"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+}
